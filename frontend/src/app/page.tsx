3be32590c4319c5e7f9e017acd5323ca75911320
rahmatjon <tom98john@gmail.com>
2025-09-24 22:17:22 +0500

    first commit

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      salom
+      <Image
+        className="dark:invert"
+        src="/next.svg"
+        alt="Next.js logo"
+        width={180}
+        height={38}
+        priority
+      />
+    </div>
+  );
+}
